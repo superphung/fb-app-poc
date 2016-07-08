708766019c688994d76fd398fc82790d7e204446
superphung <superphung44@gmail.com>
2016-07-08 14:25:52 +0200

    fix post

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static('public'));
 
+app.post('/*', function(request, response) {
+  response.redirect('/');
+});
+
 app.listen(process.env.PORT || 80, function() {
   console.log('listen on port', process.env.PORT || 80);
 })
